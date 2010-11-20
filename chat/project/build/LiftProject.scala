0ea2399b17a9d9789f884c8737d2caa55ac2dcce
David Pollak <feeder.of.the.bears@gmail.com>
2010-11-20 07:34:21 -0800

    Initial commit

---
+++ chat/project/build/LiftProject.scala
@@ -0,0 +1,23 @@
+import sbt._
+
+class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
+  val liftVersion = "2.2-SNAPSHOT"
+
+  // uncomment the following if you want to use the snapshot repo
+  // val scalatoolsSnapshot = ScalaToolsSnapshots
+
+  val dppsLocalMavenRepo = "m2" at "file:///home/dpp/.m2/repository"
+
+  // If you're using JRebel for Lift development, uncomment
+  // this line
+  override def scanDirectories = Nil
+
+  override def libraryDependencies = Set(
+    "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
+    "net.liftweb" %% "lift-mapper" % liftVersion % "compile->default",
+    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
+    "junit" % "junit" % "4.5" % "test->default",
+    "org.scala-tools.testing" %% "specs" % "1.6.5" % "test->default",
+    "com.h2database" % "h2" % "1.2.138"
+  ) ++ super.libraryDependencies
+}
---
+++ chat/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -0,0 +1,74 @@
+package bootstrap.liftweb
+
+import net.liftweb._
+import util._
+import Helpers._
+
+import common._
+import http._
+import sitemap._
+import Loc._
+import mapper._
+
+import code.model._
+
+
+/**
+ * A class that's instantiated early and run.  It allows the application
+ * to modify lift's environment
+ */
+class Boot {
+  def boot {
+    if (!DB.jndiJdbcConnAvailable_?) {
+      val vendor = 
+	new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
+			     Props.get("db.url") openOr 
+			     "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
+			     Props.get("db.user"), Props.get("db.password"))
+
+      LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
+
+      DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
+    }
+
+    // Use Lift's Mapper ORM to populate the database
+    // you don't need to use Mapper to use Lift... use
+    // any ORM you want
+    Schemifier.schemify(true, Schemifier.infoF _, User)
+
+    // where to search snippet
+    LiftRules.addToPackages("code")
+
+    // Build SiteMap
+    val entries = List(
+      Menu.i("Home") / "index", // the simple way to declare a menu
+
+      // more complex because this menu allows anything in the
+      // /static path to be visible
+      Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
+	       "Static Content"))) :::
+    // the User management menu items
+    User.sitemap
+
+    // set the sitemap.  Note if you don't want access control for
+    // each page, just comment this line out.
+    LiftRules.setSiteMap(SiteMap(entries:_*))
+
+    //Show the spinny image when an Ajax call starts
+    LiftRules.ajaxStart =
+      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+    
+    // Make the spinny image go away when it ends
+    LiftRules.ajaxEnd =
+      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
+
+    // Force the request to be UTF-8
+    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
+
+    // What is the function to test if a user is logged in?
+    LiftRules.loggedInTest = Full(() => User.loggedIn_?)
+
+    // Make a transaction span the whole HTTP request
+    S.addAround(DB.buildLoanWrapper)
+  }
+}
