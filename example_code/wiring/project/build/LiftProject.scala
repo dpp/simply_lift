48b305de7263a59c776ac1edf7d1ca0a5f0b18f9
David Pollak <feeder.of.the.bears@gmail.com>
2010-12-06 17:16:41 -0800

    Added wiring project and split wiring into separate chapter

---
+++ example_code/wiring/project/build/LiftProject.scala
@@ -0,0 +1,19 @@
+import sbt._
+
+class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
+  val liftVersion = "2.2-SNAPSHOT"
+
+  // uncomment the following if you want to use the snapshot repo
+  val scalatoolsSnapshot = ScalaToolsSnapshots
+
+  // If you're using JRebel for Lift development, uncomment
+  // this line
+  // override def scanDirectories = Nil
+
+  override def libraryDependencies = Set(
+    "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
+    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
+    "junit" % "junit" % "4.5" % "test->default",
+    "org.scala-tools.testing" %% "specs" % "1.6.6" % "test->default"
+  ) ++ super.libraryDependencies
+}
