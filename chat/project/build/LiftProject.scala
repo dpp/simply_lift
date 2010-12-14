283c0dc8fc02c2e9f493081a937cba3660002540
David Pollak <feeder.of.the.bears@gmail.com>
2010-12-14 15:52:14 -0800

    Updated the chat example to Lift 2.2-RC1

--- chat/project/build/LiftProject.scala
+++ chat/project/build/LiftProject.scala
@@ -1,10 +1,10 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val liftVersion = "2.2-SNAPSHOT"
+  val liftVersion = "2.2-RC1"
 
   // uncomment the following if you want to use the snapshot repo
-  val scalatoolsSnapshot = ScalaToolsSnapshots
+  // val scalatoolsSnapshot = ScalaToolsSnapshots
 
   // val dppsLocalMavenRepo = "m2" at "file:///home/dpp/.m2/repository"
 
@@ -16,6 +16,6 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
     "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     "junit" % "junit" % "4.5" % "test->default",
-    "org.scala-tools.testing" %% "specs" % "1.6.5" % "test->default"
+    "org.scala-tools.testing" %% "specs" % "1.6.6" % "test->default"
   ) ++ super.libraryDependencies
 }
