c09e1df5260a8c0e21894cf491ad548dc519e679
David Pollak <feeder.of.the.bears@gmail.com>
2010-12-30 10:07:10 -0800

    Updated chat to 2.2-RC5

--- chat/project/build/LiftProject.scala
+++ chat/project/build/LiftProject.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val liftVersion = "2.2-RC1"
+  val liftVersion = "2.2-RC5"
 
   // uncomment the following if you want to use the snapshot repo
   // val scalatoolsSnapshot = ScalaToolsSnapshots
