dac88056c71750803b96bf795a26a105014c5415
David Pollak <feeder.of.the.bears@gmail.com>
2010-12-31 07:26:46 -0800

    Some document cleanup.  Added skel of forms chapter

---
+++ samples/forms/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -0,0 +1,52 @@
+package bootstrap.liftweb
+
+import net.liftweb._
+import util._
+import Helpers._
+
+import common._
+import http._
+import sitemap._
+import Loc._
+
+/**
+ * A class that's instantiated early and run.  It allows the application
+ * to modify lift's environment
+ */
+class Boot {
+  /**
+   * Calculate if the page should be displayed.
+   * In this case, it will be visible every other minute
+   */
+  def displaySometimes_? : Boolean = 
+    (millis / 1000L / 60L) % 2 == 0
+
+  def boot {
+    // where to search snippet
+    LiftRules.addToPackages("code")
+
+    // Build SiteMap
+    def sitemap(): SiteMap = SiteMap(
+      Menu.i("Home") / "index" // the simple way to declare a menu
+    )
+
+    // set the sitemap.  Note if you don't want access control for
+    // each page, just comment this line out.
+    LiftRules.setSiteMapFunc(() => sitemap())
+
+    //Show the spinny image when an Ajax call starts
+    LiftRules.ajaxStart =
+      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+    
+    // Make the spinny image go away when it ends
+    LiftRules.ajaxEnd =
+      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
+
+    // Force the request to be UTF-8
+    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
+
+    // Use HTML5 for rendering
+    LiftRules.htmlProperties.default.set((r: Req) =>
+      new Html5Properties(r.userAgent))    
+  }
+}
