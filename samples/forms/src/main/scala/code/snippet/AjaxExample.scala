52be4b5c14518332ce3dcabe259bb05a4847e36e
David Pollak <feeder.of.the.bears@gmail.com>
2011-01-03 22:19:45 -0800

    Finished forms chapter

--- samples/forms/src/main/scala/code/snippet/AjaxExample.scala
+++ samples/forms/src/main/scala/code/snippet/AjaxExample.scala
@@ -10,17 +10,32 @@ import JsCmds._
 import JE._
 import scala.xml.NodeSeq
 
-
+/**
+ * Ajax for processing... it looks a lot like the Stateful example
+ */
 object AjaxExample {
   def render = {
+    // state
     var name = ""
     var age = "0"
     val whence = S.referer openOr "/"
 
+    // our process method returns a
+    // JsCmd which will be sent back to the browser
+    // as part of the response
     def process(): JsCmd= {
+
+      // sleep for 400 millis to allow the user to
+      // see the spinning icon
       Thread.sleep(400)
+      
+      // do the matching
       asInt(age) match {
+        // display an error and otherwise do nothing
         case Full(a) if a < 13 => S.error("age", "Too young!"); Noop
+
+        // redirect to the page that the user came from
+        // and display notices on that page
         case Full(a) => {
           RedirectTo(whence, () => {
             S.notice("Name: "+name)
@@ -28,12 +43,13 @@ object AjaxExample {
           })
         }
         
+        // more errors
         case _ => S.error("age", "Age doesn't parse as a number"); Noop
       }
     }
 
+    // binding looks normal
     "name=name" #> SHtml.text(name, name = _, "id" -> "the_name") &
-    "name=age" #> SHtml.text(age, age = _) &
-    "type=submit" #> ((ns: NodeSeq) => ns ++ SHtml.hidden(process))
+    "name=age" #> (SHtml.text(age, age = _) ++ SHtml.hidden(process))
   }
 }
